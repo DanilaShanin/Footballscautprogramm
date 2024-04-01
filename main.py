31f7a1fa15c35957be50584bd7a22510f484ed7c
DanilaShanin <Danilashanin2002@gmail.com>
2024-04-01 12:05:17 +0300

    Add window
    Add window

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import tkinter as tk
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        self.init_main()
+        self.view_records()
+        
+    def init_main(self):
+        toolbar = tk.Frame(bg='#fe4240', bd=2)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        self.add_img = tk.PhotoImage(file="add.gif")
+        btn_open_dialog = tk.Button(toolbar, text='Добавить игрока', command=self.open_dialog, bg='#fe4240', bd=0,
+                                    compound=tk.TOP, image=self.add_img)
+        btn_open_dialog.pack(side=tk.LEFT)
+
+class Child(tk.Toplevel):
+    def __init__(self):
+        super().__init__(root)
+        self.init_child()
+
+    def init_child(self):
+        self.title('Добавить игрока')
+        self.geometry('1000x1000+400+300')
+        
 if __name__ == "__main__":
     root = tk.Tk()
     app = Main(root)
