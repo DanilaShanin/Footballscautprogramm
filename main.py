da351975661cdfeb0cd632ba90b3c66721eb7da0
DanilaShanin <Danilashanin2002@gmail.com>
2024-04-05 00:26:47 +0300

    new features
    new features

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from model import DB
+
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.init_main()
+        self.db = db
         self.view_records()
-        
+
     def init_main(self):
         toolbar = tk.Frame(bg='#fe4240', bd=2)
         toolbar.pack(side=tk.TOP, fill=tk.X)
@@ -15,90 +17,78 @@ class Main(tk.Frame):
                                     compound=tk.TOP, image=self.add_img)
         btn_open_dialog.pack(side=tk.LEFT)
 
-class Child(tk.Toplevel):
-    def __init__(self):
-        super().__init__(root)
-        self.init_child()
+        self.update_img = tk.PhotoImage(file='update.gif')
+        btn_edit_dialog = tk.Button(toolbar, text='Изменить', bg='#fe4240', bd=0, image=self.update_img,
+                                    compound=tk.TOP, command=self.open_update_dialog)
+        btn_edit_dialog.pack(side=tk.LEFT)
 
-        def init_child(self):
-        self.title('Добавить игрока')
-        self.geometry('1000x1000+400+300')
-        self.resizable(False, False)
+        self.delete_img = tk.PhotoImage(file='delete.gif')
+        btn_delete = tk.Button(toolbar, text='Удалить игрока', bg='#fe4240', bd=0, image=self.delete_img,
+                               compound=tk.TOP, command=self.delete_records)
+        btn_delete.pack(side=tk.LEFT)
 
-        label_name = tk.Label(self, text='Имя:')
-        label_name.place(x=100, y=25)
-        label_age = tk.Label(self, text='Возраст:')
-        label_age.place(x=100, y=50)
-        label_height = tk.Label(self, text='Рост:')
-        label_height.place(x=100, y=75)
-        label_position = tk.Label(self, text='Позиция')
-        label_position.place(x=100, y=100)
-        label_information = tk.Label(self, text='Информация:')
-        label_information.place(x=100, y=125)
-        label_citizenship = tk.Label(self, text='Гражданство:')
-        label_citizenship.place(x=100, y=150)
-        label_club = tk.Label(self, text='Клуб:')
-        label_club.place(x=100, y=175)
-        label_price = tk.Label(self, text='Цена:')
-        label_price.place(x=100, y=200)
-        label_pace = tk.Label(self, text='Скорость:')
-        label_pace.place(x=100, y=225)
-        label_shooting = tk.Label(self, text='Удар:')
-        label_shooting.place(x=100, y=250)
-        label_passing = tk.Label(self, text='Передачи:')
-        label_passing.place(x=100, y=275)
-        label_dribbling = tk.Label(self, text='Дриблинг:')
-        label_dribbling.place(x=100, y=300)
-        label_defending = tk.Label(self, text='Оборона:')
-        label_defending.place(x=100, y=325)
-        label_physicality = tk.Label(self, text='Физика:')
-        label_physicality.place(x=100, y=350)
+        self.search_img = tk.PhotoImage(file='search.gif')
+        btn_search = tk.Button(toolbar, text='Поиск', bg='#fe4240', bd=0, image=self.search_img,
+                               compound=tk.TOP, command=self.open_search_dialog)
+        btn_search.pack(side=tk.LEFT)
 
 
-        self.entry_name = ttk.Entry(self)
-        self.entry_name.place(x=200, y=25)
+        self.refresh_img = tk.PhotoImage(file='refresh.gif')
+        btn_refresh = tk.Button(toolbar, text='Обновить', bg='#fe4240', bd=0, image=self.refresh_img,
+                                compound=tk.TOP, command=self.view_records)
+        btn_refresh.pack(side=tk.LEFT)
 
-        self.entry_age = ttk.Entry(self)
-        self.entry_age.place(x=200, y=50)
+        self.Logoss_img = tk.PhotoImage(file='Logoss.gif')
+        btn_Logoss = tk.Button(toolbar, bg='#fe4240', bd=0, image=self.Logoss_img,
+                                compound=tk.TOP, command=self.view_records)
+        btn_Logoss.pack(side=tk.RIGHT)
 
-        self.entry_height = ttk.Entry(self)
-        self.entry_height.place(x=200, y=75)
+        self.tree = ttk.Treeview(self, columns=('ID', 'Name', 'age', 'height', 'position', 'information', 'citizenship',
+                                                'club', 'price', 'pace', 'shooting', 'passing','dribbling', 'defending', 'physicality')
+                                 , height=15, show='headings')
+        self.tree.column("ID", width=10, anchor=tk.CENTER)
+        self.tree.column("Name", width=150, anchor=tk.CENTER)
+        self.tree.column("age", width=55, anchor=tk.CENTER)
+        self.tree.column("height", width=55, anchor=tk.CENTER)
+        self.tree.column("position", width=150, anchor=tk.CENTER)
+        self.tree.column("information", width=400, anchor=tk.CENTER)
+        self.tree.column("citizenship", width=100, anchor=tk.CENTER)
+        self.tree.column("club", width=150, anchor=tk.CENTER)
+        self.tree.column("price", width=100, anchor=tk.CENTER)
+        self.tree.column("pace", width=100, anchor=tk.CENTER)
+        self.tree.column("shooting", width=100, anchor=tk.CENTER)
+        self.tree.column("passing", width=100, anchor=tk.CENTER)
+        self.tree.column("dribbling", width=100, anchor=tk.CENTER)
+        self.tree.column("defending", width=100, anchor=tk.CENTER)
+        self.tree.column("physicality", width=100, anchor=tk.CENTER)
 
-        self.entry_position = ttk.Entry(self)
-        self.entry_position.place(x=200, y=100)
+        self.tree.heading("ID", text='ID')
+        self.tree.heading("Name", text='Имя')
+        self.tree.heading("age", text='Возраст')
+        self.tree.heading("height", text='Рост')
+        self.tree.heading("position", text='Позиция')
+        self.tree.heading("information", text='Информация')
+        self.tree.heading("citizenship", text='Гражданство')
+        self.tree.heading("club", text='Клуб')
+        self.tree.heading("price", text='Цена')
+        self.tree.heading("pace", text='Скорость')
+        self.tree.heading("shooting", text='Удар')
+        self.tree.heading("passing", text='Предачи')
+        self.tree.heading("dribbling", text='Дриблинг')
+        self.tree.heading("defending", text='Дриблинг')
+        self.tree.heading("physicality", text='Физика')
 
-        self.entry_information = ttk.Entry(self)
-        self.entry_information.place(x=200, y=125)
 
-        self.entry_citizenship = ttk.Entry(self)
-        self.entry_citizenship.place(x=200, y=150)
 
-        self.entry_club = ttk.Entry(self)
-        self.entry_club.place(x=200, y=175)
 
-        self.entry_price = ttk.Entry(self)
-        self.entry_price.place(x=200, y=200)
 
-        self.entry_pace = ttk.Entry(self)
-        self.entry_pace.place(x=200, y=225)
 
-        self.entry_shooting = ttk.Entry(self)
-        self.entry_shooting.place(x=200, y=250)
+        self.tree.pack(side=tk.LEFT)
 
-        self.entry_passing = ttk.Entry(self)
-        self.entry_passing.place(x=200, y=275)
-
-        self.entry_dribbling = ttk.Entry(self)
-        self.entry_dribbling.place(x=200, y=300)
-
-        self.entry_defending = ttk.Entry(self)
-        self.entry_defending.place(x=200, y=325)
-
-        self.entry_physicality = ttk.Entry(self)
-        self.entry_physicality.place(x=200, y=350)
-
-    self.grab_set()
-    self.focus_set()
+        scroll = tk.Scrollbar(self, command=self.tree.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+        self.tree.configure(yscrollcommand=scroll.set)
+        
         
 if __name__ == "__main__":
     root = tk.Tk()
